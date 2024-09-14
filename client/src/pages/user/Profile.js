ba58bf64a3056434588bd4b739afa0b0cc64272b
Caden <63772723+cadencjk@users.noreply.github.com>
2024-09-14 23:22:41 +0800

    Reinstate console log

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -46,6 +46,7 @@ const Profile = () => {
         toast.success("Profile Updated Successfully");
       }
     } catch (error) {
+      console.log(error);
       toast.error("Something went wrong");
     }
   };
