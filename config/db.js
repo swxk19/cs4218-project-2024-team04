c38073cfca4afd9d0772d550fafcaa578e6703a0
Kelvin Seow <45385178+swxk19@users.noreply.github.com>
2024-10-17 04:42:30 +0800

    Revert "Add support for test db"

--- config/db.js
+++ config/db.js
@@ -1,22 +1,8 @@
 import mongoose from "mongoose";
 import colors from "colors";
-import dotenv from "dotenv";
-
-var mongoUrl = undefined
-if (process.env.NODE_ENV === "production") {
-    dotenv.config({ path: "./env" }); // default to dev server, no real prod db
-    mongoUrl = process.env.MONGO_URL
-} else if (process.env.NODE_ENV === "test") {
-    dotenv.config({ path: "./.env.test" });
-    mongoUrl = process.env.MONGO_TEST_URL
-} else {
-    dotenv.config({ path: "./.env" });
-    mongoUrl = process.env.MONGO_URL
-}
-
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(mongoUrl);
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white);
     } catch (error) {
         console.log(`Error in Mongodb ${error}`.bgRed.white);
