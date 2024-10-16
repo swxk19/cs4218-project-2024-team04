ae4a503665c16f1c3ffa9916d4232eb21c511b71
swxk19 <s.wxk19@gmail.com>
2024-10-17 00:37:07 +0800

    Handle different env file for test env

--- config/db.js
+++ config/db.js
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
 import colors from "colors";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV === "production") {
+    dotenv.config({ path: "./env" }); // default to dev server, no real prod db
+} else if (process.env.NODE_ENV === "test") {
+    dotenv.config({ path: "./.env.test" });
+} else {
+    dotenv.config({ path: "./.env" });
+}
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
